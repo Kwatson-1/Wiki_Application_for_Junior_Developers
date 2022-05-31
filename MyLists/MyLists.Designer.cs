0ce566dba2e1a6914bf80b1c40a59a01691978f4
Kyle Watson <89055246+Kwatson-1@users.noreply.github.com>
2022-05-31 13:13:50 +0800

    Initial commit

---
+++ MyLists/MyLists.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace MyLists
+{
+    partial class MyLists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxDisplay = new System.Windows.Forms.ListBox();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxDisplay
+            // 
+            this.listBoxDisplay.FormattingEnabled = true;
+            this.listBoxDisplay.Location = new System.Drawing.Point(139, 129);
+            this.listBoxDisplay.Name = "listBoxDisplay";
+            this.listBoxDisplay.Size = new System.Drawing.Size(181, 225);
+            this.listBoxDisplay.TabIndex = 0;
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Location = new System.Drawing.Point(22, 129);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisplay.TabIndex = 1;
+            this.buttonDisplay.Text = "Display";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            this.buttonDisplay.Click += new System.EventHandler(this.ButtonDisplay_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(22, 161);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(75, 23);
+            this.buttonSort.TabIndex = 2;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(220, 100);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSearch.TabIndex = 3;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(139, 100);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(22, 190);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(22, 220);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(22, 250);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 7;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // MyLists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 392);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonDisplay);
+            this.Controls.Add(this.listBoxDisplay);
+            this.Name = "MyLists";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MyLists_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxDisplay;
+        private System.Windows.Forms.Button buttonDisplay;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEdit;
+    }
+}
+
---
+++ MyLists/MyLists.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Kyle Watson
+// A List containing name, structure,category and defition of programming terms
+//
+//
+namespace MyLists
+{
+    public partial class MyLists : Form
+    {
+        public MyLists()
+        {
+            InitializeComponent();
+        }
+        List<string> myList = new List<string>() { "Blue", "Red", "Yellow", "Green", "Orange", "Purple", "Cyan", "Pink", "White", "Black", "Peach", "Gold" };
+
+        private void DisplayList()
+        {
+            listBoxDisplay.Items.Clear();
+            foreach(string item in myList)
+            {
+                listBoxDisplay.Items.Add(item);
+            }
+        }
+
+        private void ButtonDisplay_Click(object sender, EventArgs e)
+        {
+            DisplayList();
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            myList.Sort();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            myList.Sort();
+            if (myList.BinarySearch(textBoxSearch.Text) >= 0)
+            {
+                MessageBox.Show("Found");
+                textBoxSearch.Clear();
+                textBoxSearch.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Not Found");
+                textBoxSearch.Clear();
+                textBoxSearch.Focus();
+            }
+        }
+
+        private void MyLists_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                listBoxDisplay.SetSelected(listBoxDisplay.SelectedIndex, true);
+                textBoxSearch.Text = myList.ElementAt(listBoxDisplay.SelectedIndex);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            myList.Add(textBoxSearch.Text);
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            listBoxDisplay.SetSelected(listBoxDisplay.SelectedIndex, true);
+            myList.RemoveAt(listBoxDisplay.SelectedIndex);
+            DisplayList();
+            textBoxSearch.Clear();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            myList[listBoxDisplay.SelectedIndex] = textBoxSearch.Text;
+            textBoxSearch.Clear();
+            DisplayList();
+        }
+        private bool ValidName(string checkThisName)
+        {
+            if (checkThisName == null)  
+        }
+    }
+}
